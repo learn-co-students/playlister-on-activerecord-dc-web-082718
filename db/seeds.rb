28ec5627b1432a812e83c1d4f850353ef762d4e3
DeCode2018 <decode2018@gmail.com>
2018-09-10 14:05:43 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+Song.create(name:'song 1', artist_id:1, genre_id:1)
+Song.create(name:'song 2', artist_id:1, genre_id:1)
+Song.create(name:'song 3', artist_id:2, genre_id:2)
+Song.create(name:'song 4', artist_id:2, genre_id:3)
+Song.create(name:'song 5', artist_id:3, genre_id:1)
+
+
+Artist.create(name:'artist 1')
+Artist.create(name:'artist 2')
+Artist.create(name:'artist 3')
+
+Genre.create(name:'pop')
+Genre.create(name:'rap')
+Genre.create(name:'classic')
